132c5883ef4d5df647ced63971edd602cf8abaa3
AJ <alexanderjessediaz@gmail.com>
2020-07-10 13:41:58 -0600

    add authenticate method to application controller, add profile controller and model

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,21 @@
 class ProfilesController < ApplicationController
+before_action :authenticate, only: [:create]
+
     def index
         @profiles = Profile.all
 
         render json: {profiles: @profiles}
     end 
-    def create
+    def create 
+        authenticate     
+
         @profile = Profile.create(
-            first_name: params[:first_name],
-            last_name: params[:last_name],
-            email: params[:email],
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email],
         )
 
         render json: {profile: @profile}, status: :created
     end
+
 end
